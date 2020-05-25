236db4a5e94c5358e806e398f2e3a016e7307f20
Leo Dion <leogdion@brightdigit.com>
2020-05-24 22:02:52 -0400

    adding downloading and string replacement

--- Sources/eggseed/main.swift
+++ Sources/eggseed/main.swift
@@ -1,1 +1,27 @@
-print("hello world")
+import Foundation
+import ZIPFoundation
+
+var isFinished = false
+let url = URL(string: "https://github.com/brightdigit/eggseed-template/archive/master.zip")!
+URLSession.shared.downloadTask(with: url) { (destination, _, error) in
+  print(destination)
+  guard let url = destination else {
+    return
+  }
+  let archive = Archive(url: url, accessMode: .read)
+  archive?.enumerated().map({ (element) in
+    debugPrint(element.element.path, element.element.type)
+  })
+  isFinished = true
+  
+}.resume()
+
+while (!isFinished) {
+  RunLoop.main.run(until: .distantPast)
+}
+// executable or library
+// cocoapods support
+// oses
+// ci support
+// sourcedocs or jazzy
+// swiftformat or/and swiftlint
