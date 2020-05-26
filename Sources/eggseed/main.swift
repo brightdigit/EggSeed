38bc851c34af8f495feacf8b0ba3d460c952fee0
Leo Dion <leogdion@brightdigit.com>
2020-05-25 21:28:17 -0400

    working package creation

--- Sources/eggseed/main.swift
+++ Sources/eggseed/main.swift
@@ -1,17 +1,51 @@
 import Foundation
 import ZIPFoundation
+import ShellOut
 
+let filesFilter = [".github/workflows/macOS.yml"
+,".github/workflows/ubuntu.yml"
+,".travis.yml"
+,"bitrise.yml"
+,".circleci/config.yml"
+,"README.md"
+,"LICENSE",
+"Example/project.yml"]
 var isFinished = false
+let destinationFolderURL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath)
 let url = URL(string: "https://github.com/brightdigit/eggseed-template/archive/master.zip")!
+let packageName = destinationFolderURL.lastPathComponent
+let userName = URL(string: (try! shellOut(to: "git remote get-url origin")))!.deletingLastPathComponent().lastPathComponent
 URLSession.shared.downloadTask(with: url) { (destination, _, error) in
-  print(destination)
   guard let url = destination else {
     return
   }
-  let archive = Archive(url: url, accessMode: .read)
-  archive?.enumerated().map({ (element) in
-    debugPrint(element.element.path, element.element.type)
+  guard let archive = Archive(url: url, accessMode: .read) else {
+    preconditionFailure()
+  }
+  archive.enumerated().forEach({ (item) in
+    if item.element.type == .file {
+      let path = item.element.path.components(separatedBy: "/").dropFirst().joined(separator: "/")
+      let url = destinationFolderURL.appendingPathComponent(path)
+      debugPrint(path)
+      if filesFilter.contains(path) {
+        try? FileManager.default.createDirectory(at: url.deletingLastPathComponent(), withIntermediateDirectories: true, attributes: nil)
+        try! archive.extract(item.element) { (data) in
+          if var text = String(data: data, encoding: .utf8) {
+            text = text.replacingOccurrences(of: "_PACKAGE_NAME", with: packageName)
+            text = text.replacingOccurrences(of: "_USER_NAME", with: userName)
+            try! text.write(to: url, atomically: true, encoding: .utf8)
+          } else {
+            try! data.write(to: url)
+          }
+        }
+      } else {
+        try! archive.extract(item.element, to: url)
+        
+      }
+    }
   })
+  
+  try! shellOut(to: .createSwiftPackage(withType: .library))
   isFinished = true
   
 }.resume()
