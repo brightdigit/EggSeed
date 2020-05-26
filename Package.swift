1f7f3034da175c6d5ac7161a5326a1ae767f6033
leogdion <leogdion@brightdigit.com>
2020-05-26 11:10:38 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,12 +18,12 @@ let package = Package(
   dependencies: [
     // Dependencies declare other packages that this package depends on.
     // .package(url: /* package url */, from: "1.0.0"),
-    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.0.1"),
-    .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.0.0"),
-    .package(url: "https://github.com/weichsel/ZIPFoundation.git", .upToNextMajor(from: "0.9.0")),
-    .package(url: "https://github.com/brightdigit/Komondor.git", .branch("feature/platforms-2019")),
-    .package(url: "https://github.com/eneko/SourceDocs.git", from: "1.0.0"),
-    .package(url: "https://github.com/SwiftPackageIndex/PackageListValidator.git", .branch("release/0.0.1"))
+    .package(url: "https://github.com/apple/swift-argument-parser", from: "0.0.1"),
+    .package(url: "https://github.com/JohnSundell/ShellOut", from: "2.0.0"),
+    .package(url: "https://github.com/weichsel/ZIPFoundation", .upToNextMajor(from: "0.9.0")),
+    .package(url: "https://github.com/brightdigit/Komondor", .branch("feature/platforms-2019")),
+    .package(url: "https://github.com/eneko/SourceDocs", from: "1.0.0"),
+    .package(url: "https://github.com/SwiftPackageIndex/PackageListValidator", .branch("release/0.0.1"))
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
