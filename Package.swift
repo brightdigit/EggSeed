90ad5d737a18d9bb2e4169541912fbb9a93cbffd
leogdion <leogdion@brightdigit.com>
2020-12-15 15:49:25 -0500

    adding dev dependencies

--- Package.swift
+++ Package.swift
@@ -17,16 +17,17 @@ let package = Package(
     .library(name: "EggSeedKit", targets: ["EggSeedKit"])
   ],
   dependencies: [
-    // Dependencies declare other packages that this package depends on.
-    // .package(url: /* package url */, from: "1.0.0"),
     .package(url: "https://github.com/apple/swift-argument-parser", from: "0.0.1"),
     .package(url: "https://github.com/weichsel/ZIPFoundation", .upToNextMajor(from: "0.9.0")),
-    .package(url: "https://github.com/shibapm/Komondor", from: "1.0.5"),
-    .package(url: "https://github.com/eneko/SourceDocs", from: "1.0.0"),
-    .package(url: "https://github.com/SwiftPackageIndex/PackageListValidator", from: "0.1.0"),
     .package(url: "https://github.com/mxcl/PromiseKit.git", from: "7.0.0-alpha.3"),
     .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax.git", .exact("0.50200.0")),
-    .package(name: "mustache", url: "https://github.com/AlwaysRightInstitute/mustache", from: "0.5.9")
+    .package(name: "mustache", url: "https://github.com/AlwaysRightInstitute/mustache", from: "0.5.9"),
+    .package(url: "https://github.com/shibapm/Komondor", from: "1.0.6"), // dev
+    .package(url: "https://github.com/eneko/SourceDocs", from: "1.2.1"), // dev
+    .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.47.0"), // dev
+    .package(url: "https://github.com/realm/SwiftLint", from: "0.41.0"), // dev
+    .package(url: "https://github.com/brightdigit/Rocket", .branch("feature/yams-4.0.0")), // dev
+    .package(url: "https://github.com/mattpolzin/swift-test-codecov", .branch("master")) // dev
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
